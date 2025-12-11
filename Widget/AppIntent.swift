d3284b1ce6a0ccdc8f8a48b98a092f9e35516d0d
alexlam0206 <nok@pgmiv.com>
2025-12-11 18:57:50 +0800

    Bug fix

---
+++ Widget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  Widget
+//
+//  Created by Alex Lam on 10/12/2025.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ Widget/WidgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  WidgetBundle.swift
+//  Widget
+//
+//  Created by Alex Lam on 10/12/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Widget()
+        WidgetControl()
+        WidgetLiveActivity()
+    }
+}
